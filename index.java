15af32b8b143888e782d2aebb970c498eb3eb992
TomFiume100 <tomdiamonds100@gmail.com>
2020-10-02 14:36:20 +0000

    I made something I think it looks cool.

--- index.java
+++ index.java
@@ -1,13 +1,13 @@
 void setup() {
-	size(300, 300);
+	size(500, 500);
+  println("Left or right click to draw a rectangle. Press any key to reset the background.")
+  background(255);
 }
-
-void draw(){
-    
-
-
-
-
+void mousePressed(){
+  stroke(0)
+  fill(mouseX,mouseY,0)
+  rect(mouseX,mouseY,mouseX,mouseY)
 }
-
-//dont forget to add mouse and keypressed interaction functions
+void keyPressed(){
+  background(255);
+}
